ee151ab6c5a0384da3efa560e48f1c21d51421e4
Rodrigo Queiroz <rodrigo.queiroz@deltatre.com>
2023-06-30 11:02:00 +0100

    increase retry delay of decoder re-init due to issues with TCL

--- library/core/src/main/java/com/google/android/exoplayer2/mediacodec/MediaCodecRenderer.java
+++ library/core/src/main/java/com/google/android/exoplayer2/mediacodec/MediaCodecRenderer.java
@@ -1005,8 +1005,8 @@ public abstract class MediaCodecRenderer extends BaseRenderer {
             // If creating the preferred decoder failed then sleep briefly before retrying.
             // Workaround for [internal b/191966399].
             // See also https://github.com/google/ExoPlayer/issues/8696.
-            Log.w(TAG, "Preferred decoder instantiation failed. Sleeping for 50ms then retrying.");
-            Thread.sleep(/* millis= */ 50);
+            Log.w(TAG, "Preferred decoder instantiation failed. Sleeping for 1000ms then retrying.");
+            Thread.sleep(/* millis= */ 1000);
             initCodec(codecInfo, crypto);
           } else {
             throw e;
